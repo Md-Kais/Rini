4cab04d5463a02de96abd373acfe1ba25228eb00
Kais <markkais129@gmail.com>
2023-02-16 03:23:32 +0600

    update rini

--- app.js
+++ app.js
@@ -6,17 +6,19 @@ const users = [
     { name: 'Kais', age: 16 },
     { name: 'Pinky', age: 18 },
     { name: 'Rohit', age: 18 },
-    { name: 'polaroid', age: 19}
+    { name: 'polaroid', age: 19 },
+    { name: 'Imran', age: 21 }
+
 
 ];
 
 app.get('/', (req, res) => {
     res.send({
-        msg: 'Rini is here',~
-        user: {}
+        msg: 'Rini is here',
+    user: {}
     });
     ///200 -> ok
-    //404 -> not found
+    //404 -> not found~
     //403 -> forbidden
 
 });
